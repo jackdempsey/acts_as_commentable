247df985a4c0419850dac1affb387ef7b8267e34
Jack Dempsey <jack.dempsey@gmail.com>
2010-03-27 09:11:33 -0400

    remove default of '' for comment  column

--- lib/generators/comment/templates/create_comments.rb
+++ lib/generators/comment/templates/create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration
   def self.up
     create_table :comments do |t|
       t.string :title, :limit => 50, :default => "" 
-      t.text :comment, :default => "" 
+      t.text :comment
       t.references :commentable, :polymorphic => true
       t.references :user
       t.timestamps
